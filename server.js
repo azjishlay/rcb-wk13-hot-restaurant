d1d23ed75ab4e3320bb8ec61cef2a1d3bde8ee86
Jasmine Li <li.jasmine.j@gmail.com>
2016-04-23 13:10:53 -0400

    Updated server

---
+++ server.js
@@ -0,0 +1,29 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var routes = require(".app/routes/routes.js");
+
+var app = express();
+
+var PORT = process.env.PORT || 8081;  
+// set the port
+
+var staticContentFolder;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+staticContentFolder = __dirname + '/public';
+
+app.use(express.static(staticContentFolder));
+// set the static files location 
+// /public/img will be /img for users
+
+app.listen(PORT,function() {
+    console.log(
+      'Serving static content from ' + 
+      staticContentFolder);
+    console.log(
+      'App listenting on PORT: ' + 
+      PORT);
+});
